6c7775d533b1828fc8599f3d29eed36b3ec60854
KowComical <xuanren1992@yahoo.com>
2022-07-30 02:34:11 +0800

    fix bug

--- code/global_code/all_sum.py
+++ code/global_code/all_sum.py
@@ -42,6 +42,7 @@ def process():
 
     df_all = df_all.drop(columns=['utc'])
     df_all['date'] = df_all['date'].dt.strftime('%d/%m/%Y')
+    df_all = df_all[['state', 'date', 'sector', 'value', 'timestamp']]  # 不这样做就会有bug 我也不知道为什么
     df_all.to_csv(os.path.join(out_path, 'all_data.csv'), index=False, encoding='utf_8_sig')
 
 
--- code/global_code/auto_upload.py
+++ code/global_code/auto_upload.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 import logging
 
 logging.getLogger('WDM').setLevel(logging.NOTSET)  # 关闭运行chrome时的打印内容
