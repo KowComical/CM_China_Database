d4001e515dbbcf8732576c91854d5e74bd54c28a
KowComical <xuanren1992@yahoo.com>
2022-07-30 04:11:56 +0800

    Update aviation.py

--- code/Aviation/aviation.py
+++ code/Aviation/aviation.py
@@ -25,15 +25,15 @@ def process():
     df_daily['date'] = pd.to_datetime(df_daily['date'], format='%d/%m/%Y')
     df_daily['year'] = df_daily['date'].dt.year
 
-    # 省级航空比例的数据 # 这个不知道怎么来的 而且缺2022年的数据
-    df_ratio = pd.read_csv(os.path.join(raw_path, 'Aviation_Provincial Ratio_2019_2021.csv'), encoding='GB2312')
+    # 省级航空比例的数据
+    df_ratio = pd.read_csv(os.path.join(raw_path, 'ratio.csv'))
     df_city = pd.read_csv(os.path.join(useful_path, 'city_name.csv'))
     df_ratio = pd.merge(df_ratio, df_city, left_on='Province', right_on='中文')[['拼音', '2019', '2020', '2021']]  # 城市改英文
     df_ratio = df_ratio.set_index(['拼音']).stack().reset_index().rename(
         columns={'level_1': 'year', 0: 'ratio', '拼音': 'state'})
     df_ratio['year'] = df_ratio['year'].astype(int)
 
-    # 乘以ratio 但是少2022年
+    # 乘以ratio
     df_result = pd.merge(df_daily, df_ratio)
     df_result['value'] = df_result['value'] * df_result['ratio'] / 100
 
