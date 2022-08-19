b79b61c8c839f57835ac471d8eb903a1a12d1063
KowComical <xuanren1992@yahoo.com>
2022-08-19 11:28:58 +0800

    Update all_process.py

--- code/global_code/all_process.py
+++ code/global_code/all_process.py
@@ -5,49 +5,49 @@ import sys
 
 sys.dont_write_bytecode = True
 
-# try:
-#     sys.path.append('./code/global_code/')
-#     print('#######################')
-#     print('Begin process Download Daily...')
-#     import download_daily as dd
-#
-#     dd.main()
-#     print('Finish process Download Daily...')
-# except Exception as e:
-#     print(e)
+try:
+    sys.path.append('./code/global_code/')
+    print('#######################')
+    print('Begin process Download Daily...')
+    import download_daily as dd
 
-# try:
-#     sys.path.append('./code/Aviation/')
-#     print('#######################')
-#     print('Begin process Aviation...')
-#     import aviation as av
-#
-#     av.main()
-#     print('Finish process Aviation...')
-# except Exception as e:
-#     print(e)
+    dd.main()
+    print('Finish process Download Daily...')
+except Exception as e:
+    print(e)
 
-# try:
-#     sys.path.append('./code/Ground_Transport/')
-#     print('#######################')
-#     print('Begin process Ground Transposrt...')
-#     import ground_transport as gt
-#
-#     gt.main()
-#     print('Finish process Ground Transposrt...')
-# except Exception as e:
-#     print(e)
-#
-# try:
-#     sys.path.append('./code/Industry/')
-#     print('#######################')
-#     print('Begin process Industry...')
-#     import industry as i
-#
-#     i.main()
-#     print('Finish process Industry...')
-# except Exception as e:
-#     print(e)
+try:
+    sys.path.append('./code/Aviation/')
+    print('#######################')
+    print('Begin process Aviation...')
+    import aviation as av
+
+    av.main()
+    print('Finish process Aviation...')
+except Exception as e:
+    print(e)
+
+try:
+    sys.path.append('./code/Ground_Transport/')
+    print('#######################')
+    print('Begin process Ground Transposrt...')
+    import ground_transport as gt
+
+    gt.main()
+    print('Finish process Ground Transposrt...')
+except Exception as e:
+    print(e)
+
+try:
+    sys.path.append('./code/Industry/')
+    print('#######################')
+    print('Begin process Industry...')
+    import industry as i
+
+    i.main()
+    print('Finish process Industry...')
+except Exception as e:
+    print(e)
 
 try:
     sys.path.append('./code/Power/')
@@ -59,25 +59,25 @@ try:
     print('Finish process Power...')
 except Exception as e:
     print(e)
-#
-# try:
-#     sys.path.append('./code/Residential/')
-#     print('#######################')
-#     print('Begin process Residential...')
-#     import residential as re
-#
-#     re.main()
-#     print('Finish process Residential...')
-# except Exception as e:
-#     print(e)
-#
-# try:
-#     sys.path.append('./code/global_code/')
-#     print('#######################')
-#     print('Begin process All Sum...')
-#     import all_sum as alls
-#
-#     alls.main()
-#     print('Finish process All Sum...')
-# except Exception as e:
-#     print(e)
+
+try:
+    sys.path.append('./code/Residential/')
+    print('#######################')
+    print('Begin process Residential...')
+    import residential as re
+
+    re.main()
+    print('Finish process Residential...')
+except Exception as e:
+    print(e)
+
+try:
+    sys.path.append('./code/global_code/')
+    print('#######################')
+    print('Begin process All Sum...')
+    import all_sum as alls
+
+    alls.main()
+    print('Finish process All Sum...')
+except Exception as e:
+    print(e)
