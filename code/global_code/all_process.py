18f997ed7ff1928007b8763b5b713737ce7f86ba
KowComical <xuanren1992@yahoo.com>
2022-08-20 14:37:04 +0800

    Update all_process.py

--- code/global_code/all_process.py
+++ code/global_code/all_process.py
@@ -27,49 +27,49 @@ try:
 except Exception as e:
     print(e)
 
-try:
-    sys.path.append('./code/Ground_Transport/')
-    print('#######################')
-    print('Begin process Ground Transposrt...')
-    import ground_transport as gt
-
-    gt.main()
-    print('Finish process Ground Transposrt...')
-except Exception as e:
-    print(e)
-
-try:
-    sys.path.append('./code/Industry/')
-    print('#######################')
-    print('Begin process Industry...')
-    import industry as i
-
-    i.main()
-    print('Finish process Industry...')
-except Exception as e:
-    print(e)
-
-try:
-    sys.path.append('./code/Power/')
-    print('#######################')
-    print('Begin process Power...')
-    import power as p
-
-    p.main()
-    print('Finish process Power...')
-except Exception as e:
-    print(e)
-
-try:
-    sys.path.append('./code/Residential/')
-    print('#######################')
-    print('Begin process Residential...')
-    import residential as re
-
-    re.main()
-    print('Finish process Residential...')
-except Exception as e:
-    print(e)
+# try:
+#     sys.path.append('./code/Ground_Transport/')
+#     print('#######################')
+#     print('Begin process Ground Transposrt...')
+#     import ground_transport as gt
+#
+#     gt.main()
+#     print('Finish process Ground Transposrt...')
+# except Exception as e:
+#     print(e)
+#
+# try:
+#     sys.path.append('./code/Industry/')
+#     print('#######################')
+#     print('Begin process Industry...')
+#     import industry as i
+#
+#     i.main()
+#     print('Finish process Industry...')
+# except Exception as e:
+#     print(e)
+#
+# try:
+#     sys.path.append('./code/Power/')
+#     print('#######################')
+#     print('Begin process Power...')
+#     import power as p
+#
+#     p.main()
+#     print('Finish process Power...')
+# except Exception as e:
+#     print(e)
+#
+# try:
+#     sys.path.append('./code/Residential/')
+#     print('#######################')
+#     print('Begin process Residential...')
+#     import residential as re
+#
+#     re.main()
+#     print('Finish process Residential...')
+# except Exception as e:
+#     print(e)
 
 try:
     sys.path.append('./code/global_code/')
